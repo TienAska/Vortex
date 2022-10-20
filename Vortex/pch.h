f3018ba6ea4e3baa2da6d92bbd7f5cf0ee9f5637
TienAska <constantine.sky@hotmail.com>
2022-10-20 16:28:51 +0800

    Add project files.

---
+++ Vortex/pch.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12.h"
+
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Hosting.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
+#include <Windows.UI.Xaml.Hosting.DesktopWindowXamlSource.h>
