7d46da87c0816fb200400a12a18589027752543c
杨天 <yangtian01@corp.netease.com>
2023-08-02 18:55:07 +0800

    Add DXTK12 package and remove the previous files.

--- Vortex/pch.h
+++ Vortex/pch.h
@@ -6,8 +6,8 @@
 #include <dxgi1_6.h>
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
-#include "SimpleMath.h"
-#include "d3dx12.h"
+#include <SimpleMath.h>
+#include <d3dx12.h>
 
 #ifdef GetCurrentTime
 #undef GetCurrentTime
