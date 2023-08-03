7665f49ca0643525faf63603d8bc23d650ce7d79
杨天 <yangtian01@corp.netease.com>
2023-08-03 11:11:21 +0800

    Add Flecs and update toolset version.

--- Vortex/pch.h
+++ Vortex/pch.h
@@ -2,13 +2,6 @@
 
 #include <windows.h>
 
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <D3Dcompiler.h>
-#include <DirectXMath.h>
-#include <SimpleMath.h>
-#include <d3dx12.h>
-
 #ifdef GetCurrentTime
 #undef GetCurrentTime
 #endif
@@ -20,3 +13,12 @@
 #include <winrt/Windows.UI.Xaml.Hosting.h>
 #include <winrt/Windows.UI.Xaml.Media.h>
 #include <Windows.UI.Xaml.Hosting.DesktopWindowXamlSource.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <SimpleMath.h>
+#include <d3dx12.h>
+
+#include "flecs.h"
