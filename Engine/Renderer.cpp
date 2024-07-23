710a995130060887dd549a70932e380a06690c25
TienAska <constantine.sky@hotmail.com>
2024-07-22 22:55:48 -0400

    Input controls the model matrix.

--- Engine/Renderer.cpp
+++ Engine/Renderer.cpp
@@ -165,6 +165,24 @@ void Vortex::Renderer::Update()
 		if (reading->GetKeyState(static_cast<uint32_t>(keyState.size()), keyState.data()))
 		{
 			//UploadTexture();
+			if (keyState.front().virtualKey == ' ')
+			{
+				m_globalParams->model = DirectX::XMMatrixIdentity();
+			}
+			else if(keyState.front().virtualKey == 'A')
+			{
+				m_globalParams->model = DirectX::XMMatrixTranspose(DirectX::XMMatrixTranslationFromVector(DirectX::XMVectorSet(-1.0f, 0.0f, 0.0f, 1.0f)));
+			}
+			m_globalParams->view = DirectX::XMMatrixTranspose(DirectX::XMMatrixLookAtLH({ 0.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 1.0f }));
+			m_globalParams->projection = DirectX::XMMatrixTranspose(DirectX::XMMatrixPerspectiveFovLH(90.0f, 1.0f, 0.01f, 1000.0f));
+			m_globalParams->time = std::chrono::duration<float>(std::chrono::steady_clock::now() - m_timeSinceStart).count();
+			
+
+			uint8_t* gpuPtr = nullptr;
+			CD3DX12_RANGE range(0, 0);
+			winrt::check_hresult(m_constantResource->Map(0, &range, reinterpret_cast<void**>(&gpuPtr)));
+			memcpy(gpuPtr, m_globalParams.get(), sizeof(GlobalParameters));
+			m_constantResource->Unmap(0, nullptr);
 		}
 
 		GameInputMouseState mouseState;
@@ -201,14 +219,4 @@ void Vortex::Renderer::Update()
 		m_gameDevice->Release();
 		m_gameDevice.detach();
 	}
-
-    uint8_t* gpuPtr = nullptr;
-    CD3DX12_RANGE range(0, 0);
-    winrt::check_hresult(m_constantResource->Map(0, &range, reinterpret_cast<void**>(&gpuPtr)));
-    m_globalParams->model = DirectX::XMMatrixIdentity();
-    m_globalParams->view = DirectX::XMMatrixTranspose(DirectX::XMMatrixLookAtLH({ 0.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 1.0f }));
-    m_globalParams->projection = DirectX::XMMatrixTranspose(DirectX::XMMatrixPerspectiveFovLH(90.0f, 1.0f, 0.01f, 1000.0f));
-    m_globalParams->time = std::chrono::duration<float>(std::chrono::steady_clock::now() - m_timeSinceStart).count();
-    memcpy(gpuPtr, m_globalParams.get(), sizeof(GlobalParameters));
-    m_constantResource->Unmap(0, nullptr);
 }
